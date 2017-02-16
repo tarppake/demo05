3a965df94eaa7f4d9c370a03b953d3e9ad76a0b6
taruk <taruk@DESKTOP-5G22CPF>
2017-02-16 10:07:37 +0200

    teht1

---
+++ CdApplication/CD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CdApplication
+{
+    class CD
+    {
+
+        public string Cd { get; set; }
+
+
+    }
+}
